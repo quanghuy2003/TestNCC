56ed79db2f27cd789db6a8cb207d0ef954db391a
quanghuy2002 <hoangqunghuy753@gmail.com>
2023-03-28 11:27:12 +0700

    test TTS

---
+++ src/Quiz.java
@@ -0,0 +1,31 @@
+public class Quiz {
+    //Quiz1:
+    //    public static void main(String[] args) {
+    //        int n = 1000000;
+    //        System.out.println(tribonacci(n));
+    //    }
+    //
+    //    public static long tribonacci(int n) {
+    //        long[] arr = new long[n + 1];
+    //        arr[0] = 4;
+    //        arr[1] = 7;
+    //        arr[2] = 5;
+    //        for (int i = 3; i <= n; i++) {
+    //            arr[i] = arr[i - 1] + arr[i - 2] + arr[i - 3];
+    //        }
+    //        return arr[n];
+    //    }
+
+
+
+
+    //Quiz2:
+    // SELECT Country, COUNT(*) AS NumOfFemaleCustomers
+    //FROM customers
+    //WHERE Sex = false
+    //GROUP BY Country
+    //HAVING COUNT(*) > 5;
+}
+
+
+
